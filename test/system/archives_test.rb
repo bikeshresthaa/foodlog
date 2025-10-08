66f5966beee1d2e4d6f74f765d406484ffffefef
bikeshresthaa <eshbikesh@gmail.com>
2025-10-08 16:19:22 +0545

    Remove archives test

--- test/system/archives_test.rb
+++ test/system/archives_test.rb
@@ -3,18 +3,6 @@ require "application_system_test_case"
 class ArchivesTest < ApplicationSystemTestCase
   # test "visiting the index" do
   #   visit archives_url
-  #
   #   assert_selector "h1", text: "Archives"
   # end
-  setup do
-    @entry = entries(:rice)
-  end
-
-  test "visiting the archives index" do
-    visit archives_index_url
-
-    assert_text "Foodlog"
-    assert_selector "h3", text: "Entries for #{@entry.day}"
-    assert text: @entry.meal_type
-  end
 end
