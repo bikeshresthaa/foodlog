95f13caeb881112e54c84e4ed73d98c24fbc0e86
bikeshresthaa <eshbikesh@gmail.com>
2025-10-08 10:30:15 +0545

    test: complete model, controller & system test

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: "kera", carbohydrates: 1, proteins: 20, fats: 2, calories: 400)
+    assert entry.save
+  end
+
   # test "the truth" do
   #   assert true
   # end
---
+++ test/system/archives_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class ArchivesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit archives_url
+  #
+  #   assert_selector "h1", text: "Archives"
+  # end
+  setup do
+    @entry = entries(:kera)
+  end
+
+  test "visiting the archives index" do
+    visit archives_index_url
+
+    assert_text "Foodlog"
+    assert_selector "h3", text: "Entries for #{@entry.day.squish}"
+    assert text: @entry.meal_type
+  end
+end
